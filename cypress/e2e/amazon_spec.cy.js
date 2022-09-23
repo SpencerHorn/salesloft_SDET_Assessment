854b92ac5722761bb56dc69ae220d2eb374f2aa2
Spencer Horn <spencer.horn@davinci-ed.com>
2022-09-23 11:29:21 -0400

    updated tests to include filtering by items with Free Shipping

--- cypress/e2e/amazon_spec.cy.js
+++ cypress/e2e/amazon_spec.cy.js
@@ -16,23 +16,21 @@ describe('Amazon search', () => {
       cy.get('#nav-search-submit-button').click()
       // Verify the search bar and category selected for search are still populated
       cy.get('select#searchDropdownBox option:selected').should('have.text', 'Industrial & Scientific')
-      // TODO: cy.get('#twotabsearchtextbox').contains('Magnifying Glass')
-      // Select ‘Prime’ checkbox from filter menu
-      // Verify all items listed for “Magnifying Glass” are for prime delivery only
-      // ^^ Only available if you login I have sent question to Sidney
+      // Select Eligible for Free Shipping checkbox from filter menu
+      cy.get('input[type="checkbox"]').eq(0).check({ force: true })
       // Select a “Magnifying Glass” to purchase
-      cy.get('[data-index="5"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section > .s-image').click()
+      cy.get('[data-asin="B09TZSXT54"]').eq(0).click()
       // Add QTY of '7' Magnifying Glass to cart
       cy.get('#quantity').select('7',{force: true})
       cy.get('#add-to-cart-button').click()
       // Verify ‘Added to Cart’ displays
-      cy.get('#attachDisplayAddBaseAlert > .a-box-inner > .a-alert-heading').contains('Added to Cart')
+      cy.get('.a-size-medium-plus').contains('Added to Cart')
       // Select ‘Cart’
-      cy.get('#attach-sidesheet-view-cart-button').click()
+      cy.get('#sw-gtc > .a-button-inner > .a-button-text').click()
       // Verify correct quantity of items is in cart
       cy.get('#sc-subtotal-label-activecart').contains('Subtotal (7 items):')
       // Select ‘Compare with similar items’
-      cy.get('#comparison-lite-modal-B07G55FSPX').click()
+      cy.get('#comparison-lite-modal-B09TZSXT54').click()
       // Verify pop up box of similar items displays
       cy.get('#a-popover-content-2 > .a-size-large').contains('Compare with similar items')
       cy.get('.comparable_item_scroller1 > :nth-child(4) > .a-size-base').contains('Magnifying Glass')
@@ -41,30 +39,37 @@ describe('Amazon search', () => {
       cy.get('.a-button-close > .a-icon').click()
     })
 
-    it.only('As a user if I add less than 7 items to the cart, this test will fail', () => {
-      // Enter “Magnifying Glass” into the search input field
-      cy.getByAriaLabel("Search").type('Magnifying Glass')
-      //select the option “Industrial & Scientific” category from the search dropdown
-      cy.get('#twotabsearchtextbox').click().get('#searchDropdownBox').select('Industrial & Scientific',{force: true})
-      // Select Search
-      cy.get('#nav-search-submit-button').click()
-      // Verify the search bar and category selected for search are still populated
-      cy.get('select#searchDropdownBox option:selected').should('have.text', 'Industrial & Scientific')
-      // TODO: cy.get('#twotabsearchtextbox').contains('Magnifying Glass')
-      // Select ‘Prime’ checkbox from filter menu
-      // Verify all items listed for “Magnifying Glass” are for prime delivery only
-      // ^^ Only available if you login I have sent question to Sidney
-      // Select a “Magnifying Glass” to purchase
-      cy.get('[data-index="5"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section > .s-image').click()
-      // Add QTY of '7' Magnifying Glass to cart
-      // cy.get('#quantity').select('7',{force: true})
-      cy.get('#add-to-cart-button').click()
-      // Verify ‘Added to Cart’ displays
-      cy.get('#attachDisplayAddBaseAlert > .a-box-inner > .a-alert-heading').contains('Added to Cart')
-      // Select ‘Cart’
-      cy.get('#attach-sidesheet-view-cart-button').click()
-      // Verify correct quantity of items is in cart
-      cy.get('#sc-subtotal-label-activecart').contains('Subtotal (7 items):')
+    it('As a user if I add less than 7 items to the cart, this test will fail', () => {
+           // Enter “Magnifying Glass” into the search input field
+           cy.getByAriaLabel("Search").type('Magnifying Glass')
+           //select the option “Industrial & Scientific” category from the search dropdown
+           cy.get('#twotabsearchtextbox').click().get('#searchDropdownBox').select('Industrial & Scientific',{force: true})
+           // Select Search
+           cy.get('#nav-search-submit-button').click()
+           // Verify the search bar and category selected for search are still populated
+           cy.get('select#searchDropdownBox option:selected').should('have.text', 'Industrial & Scientific')
+           // Select Eligible for Free Shipping checkbox from filter menu
+           cy.get('input[type="checkbox"]').eq(0).check({ force: true })
+           // Select a “Magnifying Glass” to purchase
+           cy.get('[data-asin="B09TZSXT54"]').eq(0).click()
+           // Add QTY of '7' Magnifying Glass to cart
+           cy.get('#quantity').select('6',{force: true})
+           cy.get('#add-to-cart-button').click()
+           // Verify ‘Added to Cart’ displays
+           cy.get('.a-size-medium-plus').contains('Added to Cart')
+           // Select ‘Cart’
+           cy.get('#sw-gtc > .a-button-inner > .a-button-text').click()
+           // Verify correct quantity of items is in cart
+           // Should fail here.
+           cy.get('#sc-subtotal-label-activecart').contains('Subtotal (7 items):')
+           // Select ‘Compare with similar items’
+           cy.get('#comparison-lite-modal-B09TZSXT54').click()
+           // Verify pop up box of similar items displays
+           cy.get('#a-popover-content-2 > .a-size-large').contains('Compare with similar items')
+           cy.get('.comparable_item_scroller1 > :nth-child(4) > .a-size-base').contains('Magnifying Glass')
+           cy.get('.comparable_item_scroller2 > :nth-child(4) > .a-size-base').contains('Magnifying Glass')
+           // Close pop up box
+           cy.get('.a-button-close > .a-icon').click()
     })
 
   })
